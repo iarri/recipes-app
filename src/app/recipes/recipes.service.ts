fc224ac3b67411b4395518ada4cb5c71f4b4a12a
Isaac <iarriaga32@gmail.com>
2020-06-09 03:27:11 -0700

    Update

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesPageRoutingModule } from './recipes-routing.module';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,13 @@ export class RecipesService {
   private recipes: Recipe[] = [
     {
       id: 'r1',
-      title: 'schnitzel,',
+      title: 'schnitzel',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       ingredients:['French fries', 'pork meat', 'Salad']
     },
     {
       id: 'r2',
-      title: 'Spaghetti,',
+      title: 'Spaghetti',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Spaghetti_al_Pomodoro.JPG',
       ingredients:['spaghetti','meat','tomatoes']
     }
@@ -35,4 +36,13 @@ export class RecipesService {
       })
     };
   }
+
+
+
+
+deleteRecipe(recipeId: string){
+  this.recipes = this.recipes.filter(recipe =>{
+    return recipe.id !== recipeId;
+  });
+}
 }
